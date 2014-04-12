ed3ffe487de22d15acd39cfc9840b05ed9169d43
Tiit Pikma <pikma@ut.ee>
2014-04-12 17:00:52 +0300

    Update phenny module's __init__.py.
    * Update file header.
    * Use default value if "delay" given in config is not an integer.
    * Drop multiple bot support.

--- __init__.py
+++ __init__.py
@@ -2,61 +2,34 @@
 """
 __init__.py - Phenny Init Module
 Copyright 2008, Sean B. Palmer, inamidst.com
+Copyright 2014, Tiit Pikma
 Licensed under the Eiffel Forum License 2.
 
 http://inamidst.com/phenny/
+
 """
-
-import sys, os, time, threading, signal
 import bot
+import sys
+import time
 
-class Watcher(object):
-    # Cf. http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/496735
-    def __init__(self):
-        self.child = os.fork()
-        if self.child != 0:
-            self.watch()
-
-    def watch(self):
-        try: os.wait()
-        except KeyboardInterrupt:
-            self.kill()
-        sys.exit()
-
-    def kill(self):
-        try: os.kill(self.child, signal.SIGKILL)
-        except OSError: pass
-
-def run_phenny(config):
-    if hasattr(config, 'delay'):
-        delay = config.delay
-    else: delay = 20
-
-    def connect(config):
-        p = bot.Phenny(config)
-        p.run(config.host, config.port)
-
-    try: Watcher()
-    except Exception, e:
-        print >> sys.stderr, 'Warning:', e, '(in __init__.py)'
+def run(config):
+    delay = 20
+    if hasattr(config, "delay"):
+        if isinstance(config.delay, int):
+            delay = config.delay
+        else:
+            print >> sys.stderr, "warning: config has invalid delay, "\
+                    "defaulting to %i seconds" % delay
 
     while True:
-        try: connect(config)
+        try:
+            bot.Phenny(config).run(config.host, config.port)
         except KeyboardInterrupt:
             sys.exit()
 
-        if not isinstance(delay, int):
-            break
-
-        warning = 'Warning: Disconnected. Reconnecting in %s seconds...' % delay
-        print >> sys.stderr, warning
+        print >> sys.stderr, "warning: disconnected, "\
+                "reconnecting in %i seconds..." % delay
         time.sleep(delay)
-
-def run(config):
-    t = threading.Thread(target=run_phenny, args=(config,))
-    if hasattr(t, 'run'):
-        t.run()
-    else: t.start()
 
 if __name__ == '__main__':
     print __doc__
